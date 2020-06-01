736378237f4a8e8251ea4cb754af0301ebd69978
虾饺 <308627352@qq.com>
2020-06-01 17:12:32 +0800

    2020.6.1. 底部导航栏修改

--- lib/main.dart
+++ lib/main.dart
@@ -33,35 +33,35 @@ class Home extends StatelessWidget {
       length: 3,
       child: Scaffold(
         backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text(
-            'DemoApp',
-            textAlign: TextAlign.center,
-          ),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.search),
-                tooltip: 'Search',
-                onPressed: () => debugPrint('Search button is click')),
-          ],
-
-          elevation: 0.0,
-          //阴影设置
-          bottom: TabBar(
-              unselectedLabelColor: Colors.black38,
-              indicatorColor: Colors.black54,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 1.0,
-              //指示条的粗细调整
-              tabs: <Widget>[
-                Tab(icon: Icon(Icons.local_florist)),
-                Tab(icon: Icon(Icons.change_history)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ]),
-        ),
-        body: TabBarViewDemo(),
+//        appBar: AppBar(
+//          title: Text(
+//            'DemoApp',
+//            textAlign: TextAlign.center,
+//          ),
+//          actions: <Widget>[
+//            IconButton(
+//                icon: Icon(Icons.search),
+//                tooltip: 'Search',
+//                onPressed: () => debugPrint('Search button is click')),
+//          ],
+//
+//          elevation: 0.0, //阴影设置
+//
+//          bottom: TabBar(
+//              unselectedLabelColor: Colors.black38,
+//              indicatorColor: Colors.black54,
+//              indicatorSize: TabBarIndicatorSize.label,
+//              indicatorWeight: 1.0,
+//              //指示条的粗细调整
+//              tabs: <Widget>[
+//                Tab(icon: Icon(Icons.local_florist)),
+//                Tab(icon: Icon(Icons.change_history)),
+//                Tab(icon: Icon(Icons.directions_bike)),
+//              ]),
+//        ),
+        //body: TabBarViewDemo(),
         drawer: DrawerDemo(),
-        bottomNavigationBar:BottomNavigationBarDemo() ,
+        bottomNavigationBar:BottomNavigationBarLnt() ,
       ),
     );
   }
